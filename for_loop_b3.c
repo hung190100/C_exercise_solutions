69cc62e062592f7dbd2d1512ef3b826a899fd32d
hung190100 <45819903+hung190100@users.noreply.github.com>
2022-01-10 14:01:35 +0700

    Update for_loop_b3.c

--- for_loop_b3.c
+++ for_loop_b3.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-    for (int i = 1; i <= 9; i++) {
+    for (int i = 1; i <= 10; i++) {
         for (int j = 1; j <= 9; j++) {
             printf("%d x %d = %d\t", j, i, i*j);
         }
