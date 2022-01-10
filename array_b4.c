a007d52fc716aff0434567d85951da4ac0adc01d
hung190100 <45819903+hung190100@users.noreply.github.com>
2022-01-10 15:13:06 +0700

    Update array_b4.c

--- array_b4.c
+++ array_b4.c
@@ -6,6 +6,8 @@ int main()
     int a[10] = {7, 5, 8, 2, 3, 9, 1, 6, 8, 4};
     int temp; // bien trung gian
 
+    // Bubble Sort
+    /*
     for (int i = 0; i < 10-1; i++) {
         for (int j = 0; j < 10-1-i; j++) {
             if (a[j] < a[j+1]) {
@@ -14,6 +16,20 @@ int main()
                 a[j+1] = temp;
             }
         }
+    }
+    */
+
+    // Selection Sort
+    for (int i = 0; i < 10-1; i++) {
+        int max_id = i;
+        for (int j = i+1; j < 10; j++) {
+            if (a[j] > a[max_id]) {
+                max_id = j;
+            }
+        }
+        temp = a[max_id];
+        a[max_id] = a[i];
+        a[i] = temp;
     }
 
     printf("In mang da sap xep: ");
