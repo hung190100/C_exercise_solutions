153a9a90051c08e270c442cc056d8624b8b181d4
hung190100 <45819903+hung190100@users.noreply.github.com>
2022-01-11 20:54:58 +0700

    Update function_b1.c

--- function_b1.c
+++ function_b1.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void hoan_doi (int *a, int *b) {
+    *a = *a + *b;
+    *b = *a - *b;
+    *a = *a - *b;
+}
+
+int main()
+{
+    int a = 3, b = 4;
+    printf("Truoc khi hoan doi: a = %d, b = %d\n", a, b);
+    hoan_doi(&a, &b);
+    printf("Sau khi hoan doi: a = %d, b = %d\n", a, b);
+
+    return 0;
+}
 
