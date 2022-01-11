8237cd79024c223eab9b3c9f055611da907b4f03
hung190100 <45819903+hung190100@users.noreply.github.com>
2022-01-12 01:38:46 +0700

    Update function_b2.c

--- function_b2.c
+++ function_b2.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+void display(int n) {
+    for (int i = 1; i <= n; i++) {
+        printf("%d\n", i);
+    }
+}
+
+int main()
+{
+    int n;
+    printf("Nhap n: ");
+    scanf("%d", &n);
+    display(n);
+    return 0;
+}
