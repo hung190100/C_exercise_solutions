fc8da5163dc04e4441e93aa7237addce36edc65e
hung190100 <45819903+hung190100@users.noreply.github.com>
2022-01-12 13:13:52 +0700

    Create function_b6.c

---
+++ function_b6.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int find_max (int arr[], int n) {
+    int max = arr[0];
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    return max;
+}
+
+
+int main()
+{
+    int n;
+    printf("Nhap n: ");
+    scanf("%d", &n);
+
+    int a[n];
+
+    for (int i = 0; i < n; i++) {
+        printf("Nhap a[%d] = ", i);
+        scanf("%d", &a[i]);
+    }
+
+    printf("Gia tri lon nhat cua mang la: %d", find_max(a,n));
+
+    return 0;
+}
