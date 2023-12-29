29e6563421fa3c8319f328e0c8d33908e581e23e
SnappyS <thinhnguyentien05@gmail.com>
2023-12-29 21:17:56 +0700

    First commit

--- pointer_b4.c
+++ pointer_b4.c
@@ -51,33 +51,6 @@ int main()
     p2 = mt2;
     p3 = sum;
     p4 = mul;
-
-// Cong hai ma tran
-    if (hang_mt1 != hang_mt2 || cot_mt1 != cot_mt2) { // Yeu cau so hang va so cot cua 2 ma tran phai bang nhau
-        printf("Khong cong duoc 2 ma tran");
-    } else {
-        for (int i = 0; i < hang_mt1; i++) {
-            for (int j = 0; j < cot_mt1; j++) {
-                *(p3 + i*cot_mt1 + j) = *(p1 + i*cot_mt1 + j) + *(p2 + i*cot_mt2 + j);
-            }
-        }
-    }
-
-// Nhan hai ma tran
-    if (cot_mt1 != hang_mt2) { // Yeu cau so cot cua ma tran 1 bang so hang cua ma tran 2
-        printf("Khong nhan duoc 2 ma tran");
-    } else {
-        for (int i = 0; i < hang_mt1; i++) {
-            for (int j = 0; j < cot_mt2; j++) {
-                for (int k = 0; k < cot_mt1; k++) {
-                    *(p4 + i*cot_mt1 + j) += (*(p1 + i*cot_mt1 + k)) * (*(p2 + k*cot_mt2 + j));
-                    //mul[i][j] += mt1[i][k] * mt2[k][j];
-                }
-            }
-        }
-    }
-
-// In cac ma tran
     printf("\nIN CAC MA TRAN\n");
     printf("Ma tran 1\n");
     for (int i = 0; i < hang_mt1; i++) {
@@ -94,22 +67,43 @@ int main()
         }
         printf("\n");
     }
-
-    printf("Ma tran tong\n");
-    for (int i = 0; i < hang_mt1; i++) {
-        for (int j = 0; j < cot_mt1; j++) {
-            printf("%3d", sum[i][j]);
+// Cong hai ma tran
+    if (hang_mt1 != hang_mt2 || cot_mt1 != cot_mt2) { // Yeu cau so hang va so cot cua 2 ma tran phai bang nhau
+        printf("Khong cong duoc 2 ma tran\n");
+    } else {
+        for (int i = 0; i < hang_mt1; i++) {
+            for (int j = 0; j < cot_mt1; j++) {
+                *(p3 + i*cot_mt1 + j) = *(p1 + i*cot_mt1 + j) + *(p2 + i*cot_mt2 + j);
+            }
         }
-        printf("\n");
+        printf("Ma tran tong\n");
+            for (int i = 0; i < hang_mt1; i++) {
+                for (int j = 0; j < cot_mt1; j++) {
+                    printf("%3d", sum[i][j]);
+                }
+                printf("\n");
+            }
     }
 
-    printf("Ma tran nhan\n");
-    for (int i = 0; i < hang_mt1; i++) {
-        for (int j = 0; j < cot_mt2; j++) {
-            printf("%3d", mul[i][j]);
+// Nhan hai ma tran
+    if (cot_mt1 != hang_mt2) { // Yeu cau so cot cua ma tran 1 bang so hang cua ma tran 2
+        printf("Khong nhan duoc 2 ma tran\n");
+    } else {
+        for (int i = 0; i < hang_mt1; i++) {
+            for (int j = 0; j < cot_mt2; j++) {
+                for (int k = 0; k < cot_mt1; k++) {
+                    *(p4 + i*cot_mt1 + j) += (*(p1 + i*cot_mt1 + k)) * (*(p2 + k*cot_mt2 + j));
+                    //mul[i][j] += mt1[i][k] * mt2[k][j];
+                }
+            }
         }
-        printf("\n");
+            printf("Ma tran nhan\n");
+        for (int i = 0; i < hang_mt1; i++) {
+            for (int j = 0; j < cot_mt2; j++) {
+                printf("%3d", mul[i][j]);
+            }
+            printf("\n");
+        }
     }
-
     return 0;
 }
